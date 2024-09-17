fd66c10631f5741b762c60c9ad33700c2c96b67c
szuhaydv <szuhaydv@gmail.com>
2024-09-17 14:06:07 +0200

    init rss xml structure

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+)
+
+type Feed struct {
+	XMLName xml.Name `xml:"rss"`
+	Channel Channel  `xml:"channel"`
+}
+
+type Channel struct {
+	Title       string `xml:"title"`
+	Description string `xml:"description"`
+	Item        []Item `xml:"item"`
+}
+
+type Item struct {
+	Title       string `xml:"title"`
+	Description string `xml:"description"`
+	Link        string `xml:"link"`
+}
 
 func main() {
-  fmt.Println("This is an RSS reader project")
+	fmt.Println("This is an RSS reader project")
 }
