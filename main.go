4bbc7d956565aeec32af9e7374ac1aefd9f49c75
szuhaydv <szuhaydv@gmail.com>
2024-09-17 15:36:24 +0200

    added rss scraping

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 type Feed struct {
@@ -22,6 +24,43 @@ type Item struct {
 	Link        string `xml:"link"`
 }
 
+func printFeed(feed Feed) {
+    fmt.Println("Title:", feed.Channel.Title)
+    fmt.Println("Description:", feed.Channel.Description)
+    fmt.Println()
+    fmt.Println("Items:")
+    fmt.Println("----------------------")
+    for i, item := range feed.Channel.Item {
+        fmt.Printf("Item %d:\n", i+1)
+        fmt.Println("  Title:", item.Title)
+        fmt.Println("  Description:", item.Description)
+        fmt.Println("  Link:", item.Link)
+        fmt.Println()
+    }
+}
+
 func main() {
-	fmt.Println("This is an RSS reader project")
+  baseUrl := "https://lorem-rss.herokuapp.com/feed?unit=second&interval=30"
+
+  // GET the RSS feed (in this case Lorem RSS)
+  resp, err := http.Get(baseUrl); 
+  if err != nil {
+    fmt.Println("Error getting site", baseUrl)
+  }
+  defer resp.Body.Close()
+
+  // Read response body into memory
+  body, err := io.ReadAll(resp.Body)
+  if err != nil {
+    fmt.Println("Error reading response body")
+  }
+
+  // parse the RSS
+  var feed Feed
+  if err := xml.Unmarshal(body, &feed); err != nil {
+    fmt.Println("Failed to parse RSS feed")
+  }
+
+  printFeed(feed)
+  
 }
